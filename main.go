568f2e278b26bd84c4e1aa82ebc74bd042c98625
iyolycan <cahyo@indochat.co.id>
2020-11-30 12:12:24 +0700

    init master

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+    "fmt"
+    "strconv"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func indexHandle(w http.ResponseWriter, r *http.Request) {
+
+    d := "<html><table border=1>" ;
+
+    for y:=0; y<10; y++{
+        d += "<tr>"
+        for x:=0; x<10; x++{
+            d += "<td>" + strconv.Itoa(y) + "x" + strconv.Itoa(x) + "=" + strconv.Itoa(x*y ) + "</td>"
+        }
+        d += "</tr>"
+    }
+
+    d += "</table></html>"
+
+    fmt.Fprint(w, d)
+}
+
+func main() {
+	
+	http.HandleFunc( "/" , indexHandle)
+    http.ListenAndServe( ":80" , nil)
+}
